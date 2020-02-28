640ed8c100b597e4558efdf6fa8d92397bfc6324
brian <brianjune@naver.com>
2020-02-28 14:31:31 +0900

    쌩기초 MVT 코딩 완료!

--- dgt/blog/views.py
+++ dgt/blog/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
+#뷰에서 db에 저장된 데이터를 context로 넘겨야 하니, import하자.
+from .models import Post
+from django.utils import timezone
 
 # Create your views here.
 def post_list(request):
-  return render(request, 'blog/post_list.html', {})
+  posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+  #render(요청, 보여줄 템플릿, 넘겨줄 컨텍스트)
+  return render(request, 'blog/post_list.html', {'posts' : posts})
   
