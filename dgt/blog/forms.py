40e86f2b0c695ba84e51bd5f3d8f0f5a53f44571
brian <brianjune@naver.com>
2020-03-09 14:41:42 +0900

    cru + 인증 구현 완료

---
+++ dgt/blog/forms.py
@@ -0,0 +1,7 @@
+from django import forms
+from .models import Post
+class PostForm(forms.ModelForm):
+  #Meta 클래스는, 이 폼을 만들기 위해 어떤 모델이 쓰여야 하는지 알려주는 역할을 함.
+  class Meta:
+    model=Post
+    fields=('title','text',)
--- dgt/blog/urls.py
+++ dgt/blog/urls.py
@@ -6,4 +6,7 @@ urlpatterns=[
   #즉 localhost:8000 접속하면, views.post_list를 보여주게 됨.
   #뷰 이름은 post_list. views.py를 보면 알겠지?
   path('', views.post_list, name='post_list'),
+  path('post/<int:pk>/', views.post_detail, name='post_detail'),
+  path('post/new', views.post_new, name='post_new'),
+  path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
 ]
